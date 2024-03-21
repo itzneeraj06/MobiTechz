7fd7132e8f75ca541c530bf102d962a4546fbb60
Neeraj Chouhan <136504423+itzneeraj06@users.noreply.github.com>
2024-03-21 11:44:04 +0000

    sidebar added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './CSS/navbar.css'
 import navigation_bar from '../asset/navigation-bar.png'
 import { Link } from 'react-router-dom'
@@ -13,10 +13,25 @@ const Navbar = ({ login, setLogin }) => {
     navigate('/Login');
 
   }
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSidebarOpen(false);
+      }
+    };
+    // call function on window scroll 
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <div>
       <div className="navbar">
-        <button onClick={() => { }}><img src={navigation_bar} alt="" height='40px' width='40px' /></button>
+        <button onClick={() => toggleSidebar()}><img src={navigation_bar} alt="" height='40px' width='40px' /></button>
         <Link to="/" id='nav-heading'>MobiTechz</Link>
         <div className="search">
           <input type="text" placeholder='Text here' />
@@ -36,6 +51,23 @@ const Navbar = ({ login, setLogin }) => {
 
 
       </div>
+      {isSidebarOpen && (
+
+        <div className="sidebar">
+
+          <button onClick={toggleSidebar} className='closebutton'>Close</button>
+          <h3>Mobi<span id='spanhead'>techz</span></h3>
+            <ul>
+              
+            <li><Link to='/' onClick={()=>toggleSidebar() }className='link'>Home</Link></li>
+            <li><Link to='https://mobitechzblogs.netlify.app/' onClick={()=>toggleSidebar() }target='_blank' className='link'>Blogs</Link></li>
+            </ul>
+
+
+          
+
+        </div>
+      )}
 
 
     </div>
